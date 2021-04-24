624301ba5cbf4808d362d91da0b9f8b0b52df06b
Benny Wang <bybwang@Bennys-MacBook-Air.local>
2021-04-24 13:52:20 -0400

    database created

--- billboardapi.py
+++ billboardapi.py
@@ -13,13 +13,37 @@ def setUpDatabase(db_name):
     return cur, conn
 
 def create_bb_table(cur, conn):
-    cur.execute("CREATE TABLE IF NOT EXISTS Billboard (name TEXT PRIMARY KEY, length TEXT")
+    cur.execute('DROP TABLE IF EXISTS Billboard')
+    #cur.execute("CREATE TABLE IF NOT EXISTS Billboard (name TEXT PRIMARY KEY, length TEXT)")
+    print('database created')
     conn.commit()
 
 def get_top_songs():
     chart = billboard.ChartData('hot-100')[:25]
+    l = []
+    for entry in chart:
+        t = (entry.title, entry.artist)
+        l.append(t)
+    return l
+
+def pop_songs():
+    chart = billboard.ChartData('pop-songs')[:10]
+    l = []
+    for entry in chart:
+        t = (entry.title, entry.artist)
+        l.append(t)
+    print(l)
 
 
+def insert_songs(cur, conn, songs):
+    length = '3:00'
+    for i in songs:
+        cur.execute("INSERT INTO Billboard (name, length) VALUES (?,?)", (i[0], length))
+    conn.commit()
 def main():
     cur, conn = setUpDatabase('billboard.db')
     create_bb_table(cur, conn)
+    pop_songs()
+    #insert_songs(cur, conn, get_top_songs())
+
+main()
