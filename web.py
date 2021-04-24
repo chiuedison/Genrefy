4478a86e652bc731d3a950ac56c2e4b4ad1bbf1e
Benny Wang <bybwang@Bennys-MacBook-Air.local>
2021-04-24 13:53:40 -0400

    started creating scraper

--- web.py
+++ web.py
@@ -2,9 +2,31 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import os
+import json
 
-url = 'https://www.billboard.com/charts/dance-electronic-songs'
+url = 'https://music.apple.com/us/playlist/top-100-usa/pl.606afcbb70264d2eb2b51d8dbcfa6a12'
 r = requests.get(url)
 soup = BeautifulSoup(r.text, 'html.parser')
+rows = soup.find_all('div', class_='row track web-preview song')
 
-print(soup)
+time = rows.text.strip()
+print(r)
+
+
+
+
+'''
+url = 'https://api.lyrics.ovh/v1/Lil-Baby/On-Me'
+r = requests.get(url)
+d = json.loads(r.content)
+lyrics = d['lyrics']
+lyrics_list = lyrics.split()
+count = 0
+for word in lyrics_list:
+    count += 1
+
+print(count)'''
+
+
+
+
