24a7231b92fb811020da94078cfddf7b192cf670
Benny Wang <bybwang@Bennys-MacBook-Air.local>
2021-04-25 22:59:47 -0400

    fixed error

--- web.py
+++ web.py
@@ -26,12 +26,12 @@ def get_song_lengths(songs, url):
 
 def get_data(d):
     data = {}
-    data['Rap'] = get_song_lengths(songs, url)(d['rap'],'https://music.apple.com/us/playlist/rap-life/pl.abe8ba42278f4ef490e3a9fc5ec8e8c5')
-    data['Pop'] = get_song_lengths(songs, url)(d['pop'], 'https://music.apple.com/us/playlist/todays-hits/pl.f4d106fed2bd41149aaacabb233eb5eb')
-    data['Rock'] = get_song_lengths(songs, url)(d['rock'], 'https://music.apple.com/us/playlist/alt-pop/pl.cac8c8af1a654268973929161aba9e33')
-    data['Dance'] = get_song_lengths(songs, url)(d['dance'], 'https://music.apple.com/us/playlist/edm-hits/pl.d66feecbd40d423d81e8e643e368291a')
-    data['Country'] = get_song_lengths(songs, url)(d['country'], 'https://music.apple.com/us/playlist/todays-country/pl.87bb5b36a9bd49db8c975607452bfa2b')
-    data['Latin'] = get_song_lengths(songs, url)(d['latin'], 'https://music.apple.com/us/playlist/dale-play/pl.4b364b8b182f4115acbf6deb83bd5222')
+    data['Rap'] = get_song_lengths(d['rap'],'https://music.apple.com/us/playlist/rap-life/pl.abe8ba42278f4ef490e3a9fc5ec8e8c5')
+    data['Pop'] = get_song_lengths(d['pop'], 'https://music.apple.com/us/playlist/todays-hits/pl.f4d106fed2bd41149aaacabb233eb5eb')
+    data['Rock'] = get_song_lengths(d['rock'], 'https://music.apple.com/us/playlist/alt-pop/pl.cac8c8af1a654268973929161aba9e33')
+    data['Dance'] = get_song_lengths(d['dance'], 'https://music.apple.com/us/playlist/edm-hits/pl.d66feecbd40d423d81e8e643e368291a')
+    data['Country'] = get_song_lengths(d['country'], 'https://music.apple.com/us/playlist/todays-country/pl.87bb5b36a9bd49db8c975607452bfa2b')
+    data['Latin'] = get_song_lengths(d['latin'], 'https://music.apple.com/us/playlist/dale-play/pl.4b364b8b182f4115acbf6deb83bd5222')
     return data
 
 def unique_songs(d):
